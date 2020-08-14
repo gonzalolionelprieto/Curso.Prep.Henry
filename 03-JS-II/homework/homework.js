272aeab3b2a4ae827007fafb5333cf0d2b199c52
unknown <gonzalolionelprieto@gmail.com>
2020-08-14 00:05:42 -0300

    trabajo corregido

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -18,11 +18,11 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if idioma===aleman{
+  if (idioma==="aleman"){
   	return'Guten Tag!';
-  	} else if (idioma===mandarin) {
+  	} else if (idioma==="mandarin") {
   		return "Ni Hao!";
-  	}else if (idioma===ingles) {
+  	}else if (idioma==="ingles") {
   		return "Hello!";
   	}else {
   		return "Hola!";
